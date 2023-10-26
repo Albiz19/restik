9f4ee2745b46a81b6a558720818ba2fc6a2e6269
Nikolai <nikolai_ghromov@mail.ru>
2023-10-26 20:48:14 +0300

    test menu and product info page

--- java/urls.py
+++ java/urls.py
@@ -21,6 +21,7 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('menu/', views.menu, name='menu'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
 
 
